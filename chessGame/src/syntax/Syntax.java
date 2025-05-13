c27e53d33597442c27d14c23ecc1e43374745c69
cortes1443 <jdcortesg2@eafit.edu.co>
2025-05-12 23:33:55 -0500

    ProveMatch
    añadido metodo para verificar las partidas

--- chessGame/src/syntax/Syntax.java
+++ chessGame/src/syntax/Syntax.java
@@ -4,36 +4,59 @@ import java.util.regex.*;
 
  class Syntax {
 
-     private static String BuildSyntax() {
         //Board
-        String letter = "[a-h]";
-        String number = "[1-9]";
-        String square = "(" + number + letter + ")";
+        static String letter = "[a-h]";
+        static String number = "[1-8]";
+        static String square = "(" + letter + number + ")";
 
         //           ------GAME-------
         //Pieces
-        String piece = "[KQRBN]";
+        static String piece = "[KQRBN]";
 
         //Special moves
-        String castle = "(O-O|O-O-O)";
-        String check = "(\\+|#)?";
-        String promote = "(=[QRBN])?";
+        static String castle = "(O-O|O-O-O)";
+        static String check = "(\\+|#)?";
+        static String promote = "(=[QRBN])?";
 
         //Pieces moves
-        String advPawn = square + promote + check;
-        String pawnTakes = letter + "x" + square + promote + check;
-        String pawnMove = "(" + advPawn + "|" + pawnTakes + ")";
+        static String advPawn = square + promote + check;
+        static String pawnTakes = letter + "x" + square + promote + check;
+        static String pawnMove = "(" + advPawn + "|" + pawnTakes + ")";
 
-        String pieceMove = "(" + piece + "(" + letter + "|" + number + "|" + square + ")?x?" + square + promote + check + ")";
+        static String pieceMove = "(" + piece + "(" + letter + "|" + number + "|" + square + ")?x?" + square + promote + check + ")";
 
-        String move = "(" + castle + "|" + pawnMove + "|" + pieceMove + ")";
+        static String move = "(" + castle + "|" + pawnMove + "|" + pieceMove + ")";
+        static String gameTurn = "(\\d+)\\.\\s+([^\\s]+)(?:\\s+(?!\\d+\\.)([^\\s]+))?"; //turno (por si acaso)
+    static boolean accpt = true;
+     private static void ProveMatch(String match) {
+         Matcher matchTurns = Pattern.compile(gameTurn).matcher(match);
+         
+         
 
-         return "(\\d)+\\.\\s" + move + "\\s" + move + "?"; //turno (por si acaso)
-    }
+         while (matchTurns.find() && accpt) {
+            String turn = matchTurns.group(1);
+            String white = matchTurns.group(2);
+            String black = matchTurns.group(3); // can be null
+            
+            ProveMove(white, turn, "white");
+            if(black != null){
+                ProveMove(black, turn, "black");
+            }
+         }
+         if(accpt){
+             System.out.println("Valid match");
+         }
 
-     /*private static Boolean ProveMatch(String match) {
-         Matcher matchTurns = Pattern.compile(BuildSyntax()).matcher(match);
+     }
 
+     private static boolean ProveMove(String turnMove, String turnNum, String color){
+        if(turnMove.matches(move)){
+           return true;
+        } else {
+           System.out.println("In turn "+turnNum+", for "+color+" pieces, move "+turnMove+" invalid");
+           accpt = false;
+           return false;
+        }
+     }
 
-     }*/
 }
